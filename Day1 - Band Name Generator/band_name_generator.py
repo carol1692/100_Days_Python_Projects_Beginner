b45c6f917f597bad167e53ab8e075b2fae70bd72
Ana Lemos <carol1692@hotmail.com>
2022-12-02 15:04:49 -0300

    add first exercise

---
+++ Day1 - Band Name Generator/band_name_generator.py
@@ -0,0 +1,7 @@
+#!/usr/local/bin/python
+
+print('Welcome to the Band Name Generator.')
+born_city = input('City where you grow up:')
+pet_name = input('Write your first pet name:')
+
+print(f'The band name is: {born_city} {pet_name}')
